ea126e024f6147d218a1237f4363b9458383d908
kjell <kjell5317@gmail.com>
2021-11-06 16:05:29 +0100

    declaration

--- PDA/ÜB03/StarsDatabase.java
+++ PDA/ÜB03/StarsDatabase.java
@@ -5,12 +5,13 @@ public class StarsDatabase {
         Scanner scan = new Scanner(System.in);
 
         // setting up the database as arrays
-        String[] names = { "Sirius", "Alpha Centauri", "Rigel", "Almaaz", "Luhman 16" };
-        String[] ids = { "TYC 5949-2777-1", "TYC 9007-5849-1", "TYC 5331-1752-1", "TYC 2907-1275-1",
+        String[] names = new String[] { "Sirius", "Alpha Centauri", "Rigel", "Almaaz", "Luhman 16" };
+        String[] ids = new String[] { "TYC 5949-2777-1", "TYC 9007-5849-1", "TYC 5331-1752-1", "TYC 2907-1275-1",
                 "WISE J1049-5319A" };
-        double[] distances = { 8.6D, 4.37D, 860D, 2000D, 6.589D };
-        double[] apparentMagitude = { -1.46D, -0.27D, 0.13D, 2.92D, 14.94D };
-        String[] types = { "Main sequence", "Main sequence", "Blue supergiant", "Yellow supergiant", "Brown dwarf" };
+        double[] distances = new double[] { 8.6D, 4.37D, 860D, 2000D, 6.589D };
+        double[] apparentMagitude = new double[] { -1.46D, -0.27D, 0.13D, 2.92D, 14.94D };
+        String[] types = new String[] { "Main sequence", "Main sequence", "Blue supergiant", "Yellow supergiant",
+                "Brown dwarf" };
 
         boolean foundId = false;
         boolean foundDistance = false;
