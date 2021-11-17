5698fdce61d4ccee240b76bbc44b441a7d1521bb
kjell <kjell5317@gmail.com>
2021-11-17 17:59:42 +0100

    case

--- Programmierung/ÜB04/sudoku.java
+++ Programmierung/ÜB04/sudoku.java
@@ -1,6 +1,6 @@
 import java.util.HashSet;
 
-public class sudoku {
+public class Sudoku {
     public static void main(String[] args) { // input
         int[][] sudoku = new int[][] { { 0, 9, 0, 0, 0, 0, 1, 1, 0 }, { 8, 0, 4, 0, 2, 0, 3, 0, 7 },
                 { 0, 6, 0, 9, 0, 7, 0, 2, 0 }, { 0, 0, 5, 0, 3, 0, 1, 0, 0 }, { 0, 7, 0, 5, 0, 1, 0, 3, 0 },
