3a9826facb0556604d2b27609116bdbaa3fd89da
kjell <kjell5317@gmail.com>
2021-11-25 20:22:10 +0100

    comments

--- Programmierung/ÜB05/Matrix.java
+++ Programmierung/ÜB05/Matrix.java
@@ -1,12 +1,21 @@
+package ÜB05;
+
 public class Matrix {
     public static void main(String[] args) {
+
+        // Eingabe der Matrizen
         double[][] a = new double[][] { { 2, 1, 7 }, { 3, 0, 3 } };
         double[][] b = new double[][] { { 4, 2 }, { 7, 1 }, { 5, 2 } };
 
+        // Überprüfe auf Multiplizierbarkeit
         if (!check(a, b)) {
             System.out.println("Die Matrizen sind nicht multiplizierbar!");
         } else {
+
+            // Erstellen des Ergebnisses
             double[][] c = mult(a, b);
+
+            // Ausgabe des Ergebnisses
             for (int i = 0; i < c.length; i++) {
                 for (int j = 0; j < c[i].length; j++) {
                     System.out.print(c[i][j] + " ");
@@ -16,6 +25,7 @@ public class Matrix {
         }
     }
 
+    // Überprüfe auf Multiplizierbarkeit
     public static boolean check(double[][] a, double[][] b) {
         if (a[0].length == b.length) {
             return true;
@@ -23,15 +33,16 @@ public class Matrix {
         return false;
     }
 
+    // Multiplizeiren der Matrizen
     public static double[][] mult(double[][] a, double[][] b) {
-        double[][] c = new double[a.length][b[0].length];
+        double[][] c = new double[a.length][b[0].length]; // Länge des Ergebnis-Arrays
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b[i].length; j++) {
-                for (int k = 0; k < a[0].length; k++) {
+                for (int k = 0; k < a[0].length; k++) { // Summenformel
                     c[i][j] += a[i][k] * b[k][j];
                 }
             }
         }
-        return c;
+        return c; // Ergebnis zurückgeben
     }
 }
