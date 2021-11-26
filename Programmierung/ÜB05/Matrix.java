164f4dffe7034e32ab7dc7d9c7788cc684a2a212
kjell <kjell5317@gmail.com>
2021-11-26 12:42:22 +0100

    fix out of bound

--- Programmierung/ÜB05/Matrix.java
+++ Programmierung/ÜB05/Matrix.java
@@ -4,8 +4,8 @@ public class Matrix {
     public static void main(String[] args) {
 
         // Eingabe der Matrizen
-        double[][] a = new double[][] { { 2, 1, 7 }, { 3, 0, 3 } };
-        double[][] b = new double[][] { { 4, 2 }, { 7, 1 }, { 5, 2 } };
+        double[][] a = new double[][] { { 5, 8, 6 }, { 4, 6, 5 }, { 5, 4, 8 }, { 5, 4, 8 } };
+        double[][] b = new double[][] { { 4, 7, 9, 2 }, { 5, 8, 6, 8 }, { 6, 4, 6, 5 } };
 
         // Überprüfe auf Multiplizierbarkeit
         if (!check(a, b)) {
@@ -37,7 +37,7 @@ public class Matrix {
     public static double[][] mult(double[][] a, double[][] b) {
         double[][] c = new double[a.length][b[0].length]; // Länge des Ergebnis-Arrays
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b[i].length; j++) {
+            for (int j = 0; j < b[0].length; j++) {
                 for (int k = 0; k < a[0].length; k++) { // Summenformel
                     c[i][j] += a[i][k] * b[k][j];
                 }
