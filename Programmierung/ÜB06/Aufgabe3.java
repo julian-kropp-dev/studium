19fc969ef8e50533509007d1b504ebfa87b5886b
kjell <kjell5317@gmail.com>
2021-12-01 13:43:05 +0100

    first line first

--- Programmierung/ÜB06/Aufgabe3.java
+++ Programmierung/ÜB06/Aufgabe3.java
@@ -4,21 +4,21 @@ public class Aufgabe3 {
     public static void main(String[] args) {
 
         int[] input = { 2, 4, 1, 3, 7 };
+        stepSum(input);
         for (int i : input) {
             System.out.print(i + " ");
         }
         System.out.println();
-        stepSum(input);
     }
 
     public static void stepSum(int[] input) {
 
         int[] result = new int[input.length - 1];
+
         if (input.length > 1) {
             for (int i = 1; i < input.length; i++) {
                 result[i - 1] = input[i] + input[i - 1];
             }
-
             stepSum(result);
             for (int i = 0; i < result.length; i++) {
                 System.out.print(result[i] + " ");
