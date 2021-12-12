fbf249b359c8c391849fef18171bd7e7684965de
kjell <kjell5317@gmail.com>
2021-12-12 22:28:51 +0100

    Java Doc

--- Programmierung/UB08/Aufgabe2/Account.java
+++ Programmierung/UB08/Aufgabe2/Account.java
@@ -5,29 +5,50 @@ public class Account {
     private String owner = ""; // OWNER Value
     private double balance = 0D; // BALANCE Value
 
-    public Account(String name, double _balance) { // Constructor
+    /**
+     * Konstruktor zum Erstellen eines Kontos mit Initialwert und Owner
+     * 
+     * @param name     Owner dieses Kontos
+     * @param _balance Initialwert des Kontos
+     */
+    public Account(String name, double _balance) {
         owner = name;
         balance = _balance;
     }
 
+    /**
+     * Get Owner
+     *
+     * @return Owner eines Kontos
+     */
     public String owner() {
         return owner;
     }
 
+    /**
+     * Set Owner
+     *
+     * @param value Neuer Owner
+     */
     public void owner(String value) {
         owner = value;
     }
 
+    /**
+     * Get Balance
+     *
+     * @return Balance eines Kontos
+     */
     public double balance() {
         return balance;
     }
 
-    //// Im Beispiel ohne sinnvolle Verwendung
-    // public void balance(double value) {
-    // balance = value;
-    // }
-
-    // EINZAHLEN
+    /**
+     * Zahle Geld auf ein Konto ein
+     *
+     * @param value Betrag des einzuzahlenden Geldes
+     * @return Status des Erfolgs der Einzahlung
+     */
     public boolean einzahlen(double value) {
         if (value > 0) {
             balance += value;
@@ -36,7 +57,12 @@ public class Account {
         return false;
     }
 
-    // AUSZAHLEN
+    /**
+     * Zahle Geld eines Kontos aus
+     *
+     * @param value Betrag des auszuzahlenden Geldes
+     * @return Status des Erfolgs der Auszahlung
+     */
     public boolean auszahlen(double value) {
         if (value > 0) {
             if (balance >= value) {
@@ -47,7 +73,13 @@ public class Account {
         return false;
     }
 
-    // Transfer
+    /**
+     * Überweise Geld von einem Account auf einen anderen
+     *
+     * @param amount die Menge des zu überweisenden Geldes
+     * @param other  der Account an den das Geld überwiesen werden soll
+     * @return Status des Erfolgs der Überweisung
+     */
     public boolean transfer(double amount, Account other) {
         if (amount > 0) {
             if (other != this) {
